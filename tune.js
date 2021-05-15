a259b1f154f82fb11151331fe3d0397c66b1b778
yisusworldwide <84227371+yisusworldwide@users.noreply.github.com>
2021-05-15 13:53:17 +0200

    add-js-file-for-click-event
    Add eventListener for click alert on main  heading.

---
+++ tune.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const interact = document.querySelector('h1');
+
+    interact.addEventListener('click', () => {
+        alert("The music Quiz!");
+    })
+});
