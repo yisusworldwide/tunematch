eb44d1ca20a09c1bcf4cf9f038a2ff03e0bafcae
yisusworldwide <84227371+yisusworldwide@users.noreply.github.com>
2021-07-17 21:00:15 +0200

    tunematch-v2
    Finished JavaScript section and created header plus footer templates.

--- songs.js
+++ songs.js
@@ -2,28 +2,33 @@ const matches = [{
         song: 'Enter Sandman',
         options: { 1: 'Judas Priest', 2: 'Madonna', 3: 'Metallica' },
         images: { 1: 'https://static.dw.com/image/45116117_303.jpg', 2: 'https://alfaenlinea.com/wp-content/uploads/cache_img/Central_20210715_DocumentalMadonna-600x400-pa65eahjdaxsui93wnempcdtlcohfg07atelcorq8w.jpg', 3: 'https://www.latercera.com/resizer/jfiOqRyjM7rCirripDCQJcznbJo=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/D3G47L7XVZDEXCZSCLTDVHZRCU.jpg' },
-        correct: 3
+        correct: 3,
+        video: 'https://www.youtube.com/embed/CD-E-LDc384'
+
     },
 
     {
         song: 'Thunderstruck',
         options: { 1: 'ACDC', 2: 'Chayanne', 3: 'Motorhead' },
         images: { 1: 'https://rockfm-cdnmed.agilecontent.com/resources/jpg/8/1/1563527713918.jpg', 2: 'https://imagenes.milenio.com/KOmeluHnLIcjsZGshz_GmJ-8xj8=/958x596/smart/https://www.milenio.com/uploads/media/2020/06/28/cantante-celebro-redes-sociales-instagram_100_18_874_544.jpg', 3: 'https://estaticos-cdn.prensaiberica.es/clip/0146cc8a-df28-4adc-88bb-a5fb05a25443_16-9-aspect-ratio_default_0.jpg' },
-        correct: 1
+        correct: 1,
+        video: 'https://www.youtube.com/embed/v2AC41dglnM'
     },
 
     {
         song: 'Hablame De Ti',
         options: { 1: 'Pimpinela', 2: 'Serrat', 3: 'Pecos' },
         images: { 1: 'https://www.cmtv.com.ar/imagenes_artistas/240.jpg', 2: 'https://cdn.smehost.net/sonymusices-45pressprod/wp-content/uploads/2018/10/foto_de_joan-manuel-serrat-1.jpg', 3: 'https://www.javierdepecos.com/wp-content/uploads/2020/02/pecos-1-427x427.jpg' },
-        correct: 3
+        correct: 3,
+        video: 'https://www.youtube.com/embed/tJBrK2KAPfc'
     },
 
     {
         song: 'We Are The Champions',
         options: { 1: 'The Beatles', 2: 'Queen', 3: 'Rolling Stones' },
         images: { 1: 'https://www.show.news/__export/1593053470003/sites/debate/img/2020/06/24/the-beatles_1_crop1593051718114.jpg_423682103.jpg', 2: 'https://img2.rtve.es/a/5687219?w=400&preview=1603093247963.jpg', 3: 'https://www.plasticosydecibelios.com/wp-content/uploads/2021/07/Rolling-Stones.jpg' },
-        correct: 2
+        correct: 2,
+        video: 'https://www.youtube.com/embed/KXw8CRapg7k'
     }
 
 
